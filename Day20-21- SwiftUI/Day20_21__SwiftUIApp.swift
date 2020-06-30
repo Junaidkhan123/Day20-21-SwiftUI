3edf68117eefbf01e81cae6fdcddcf710b234852
Muhammad  Junaid <junaid123.jk78@gmail.com>
2020-06-30 15:59:05 +0500

    Initial Commit

---
+++ Day20-21- SwiftUI/Day20_21__SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day20_21__SwiftUIApp.swift
+//  Day20-21- SwiftUI
+//
+//  Created by Jhon Khan on 30/06/2020.
+//
+
+import SwiftUI
+
+@main
+struct Day20_21__SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
